cf6e6b75dda412ffcb23166fa9c35489196db1b8
Sally Resch <sallyresch27@gmail.com>
2022-07-13 16:48:54 +0200

    added an if statement

--- src/udemy/javamasterclass/Main.java
+++ src/udemy/javamasterclass/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        boolean isFood = false;
+        if (isFood == false) {
+            System.out.println("its not food, do not eat!");
+            System.out.println("And i am scared of Aliens");
+        }
     }
 }
