bcd2bbcd579a68cfea06052a02e92c5d15004d50
Sally Resch <sallyresch27@gmail.com>
2022-07-15 18:12:51 +0200

    added boolean values and tested the NOT operator

--- src/udemy/javamasterclass/Main.java
+++ src/udemy/javamasterclass/Main.java
@@ -13,5 +13,24 @@ public class Main {
         if ((topScore > 90) || (secondTopScore <= 90)){
             System.out.println("Either or both of the conditions are true");
         }
+
+    // lecture 38 Assignment Operator VS equals to Operator
+        int newValue = 50;
+        if(newValue == 50){
+            System.out.println("This is true");
+        }
+
+        //if using one equals to the sout line, this will still be printed pga it still finds a boolean value
+        boolean isCar = false;
+        if (isCar == true){
+            System.out.println("this is not supposed to happen");
+        }
+
+        // the ! or NOT operator is also known as the logical complement operator.
+        //for booleans ! tests the alternate value. we saw (isCat) tests for true, by adding a !-operator before the value we can check the opposite - false in this case
+        boolean isCat = false;
+        if (!isCat){
+            System.out.println("this is not a cat");
+        }
     }
 }
