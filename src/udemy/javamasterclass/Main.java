4611e4f0137e579c9e8ab6b0371f473e89a1a9f1
Sally Resch <sallyresch27@gmail.com>
2022-07-15 17:48:35 +0200

    added new branch and wrote some code

--- src/udemy/javamasterclass/Main.java
+++ src/udemy/javamasterclass/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int topScore = 90;
+        int secondTopScore = 60;
+        if((topScore > secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100");
+        }
+    //both && and || operates on boolean Operands - checking of a given condition is true or false.
+        if ((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true");
+        }
     }
 }
