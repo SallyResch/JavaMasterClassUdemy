837ddec6f314a3dc06f1bf18da767b07e77a3b4b
Sally Resch <sallyresch27@gmail.com>
2022-08-22 14:46:20 +0200

    made new branch 47

--- src/udemy/javamasterclass/Main.java
+++ src/udemy/javamasterclass/Main.java
@@ -8,28 +8,13 @@ public class Main {
     int levelCompleted = 5;
     int bonus = 100;
 
-    /*if (score < 5000 && score >1000) {
-        System.out.println("your score was less then 5000");
-    }else if(score <1000){
-        System.out.println("your score was less the 1000");
-    }else{
-        System.out.println("Got to 5000");
-    }*/
 
     if (gameOver){
         int finalScore = score + (levelCompleted * bonus);
         finalScore += 1000;
         System.out.println("your final score was " + finalScore);
     }
-    /*int newScore = 10000;
-    int newLevelCompleted = 8;
-    int newBonus = 200;
 
-    if(gameOver == true){
-        int newFinalScore = newScore + (newLevelCompleted * newBonus);
-        System.out.println("Your new final score was " + newFinalScore);
-
-     }*/
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
