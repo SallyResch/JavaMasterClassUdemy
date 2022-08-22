b46ff96476e4b3a0948c963c2c55c4858c0622ef
Sally Resch <sallyresch27@gmail.com>
2022-08-22 15:05:49 +0200

    method part 1 lesson

--- src/udemy/javamasterclass/Main.java
+++ src/udemy/javamasterclass/Main.java
@@ -3,23 +3,17 @@ package udemy.javamasterclass;
 public class Main {
 
     public static void main(String[] args) {
-	boolean gameOver = true;
-    int score = 800;
-    int levelCompleted = 5;
-    int bonus = 100;
 
+    calculateScore(true,800,5,100);
 
-    if (gameOver){
-        int finalScore = score + (levelCompleted * bonus);
-        finalScore += 1000;
-        System.out.println("your final score was " + finalScore);
+    calculateScore(true,10000,8,200);
     }
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+    public static void  calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         if (gameOver){
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
             System.out.println("your final score was " + finalScore);
         }
     }
